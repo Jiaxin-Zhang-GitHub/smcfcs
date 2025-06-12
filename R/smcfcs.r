b7553f644640fe18b6ae9bb3ee6e52f9eb3303b1
Jiaxin-Zhang-GitHub <65327235+Jiaxin-Zhang-GitHub@users.noreply.github.com>
2025-06-12 17:52:32 +1000

    Update smcfcs.r

--- R/smcfcs.r
+++ R/smcfcs.r
@@ -1116,6 +1116,7 @@ smcfcs.core <- function(originaldata, smtype, smformula, method, predictorMatrix
           } else {
             
             # Debug: If directImpProbs happens to be infinite, skip imputation for this iteration  
+            print("test")
             directImpProbs[is.na(directImpProbs)] <- 0  
             imputationNeeded <- imputationNeeded[rowSums(directImpProbs) > 0]
 
